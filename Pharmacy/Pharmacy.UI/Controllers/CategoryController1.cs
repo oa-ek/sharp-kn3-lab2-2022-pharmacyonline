8fc02c2e71b2b2a72e61b86151699f9990e3f629
dian-23 <diana.savchuk@oa.edu.ua>
2022-10-19 19:33:47 +0300

    Addcategorycontroller

---
+++ Pharmacy/Pharmacy.UI/Controllers/CategoryController1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Repos;
+using XStats.Core;
+
+namespace Pharmacy.UI.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        private readonly PharmacyDbContext _dbcontext;
+        private readonly CategoryRepository _categoryRepository;
+        //private readonly 
+
+        public CategoryController1(CategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CategoryCatalog(int id)
+        {
+            ViewData["id"] = id;
+            var category = _dbcontext.SubCategory.First(x => x.SubCategoryId == id);
+            ViewData["category"] = category;
+            return View(_dbcontext.Medicaments.Where(x => x.SubCategory == category).ToList());
+        }
+    }
+}
